4304e3168f93da9673cf503140eed21a29e26b91
sallyqiansun <44779808+sallyqiansun@users.noreply.github.com>
2018-11-26 19:02:30 -0500

    need help in debugging this part of code...
    hi guys, I've been trying to work on the part that prints the blanks before trying to find out a method to replace it, but it keeps printing out 15 dashes instead of 5 dashes, and I can't figure out why, if any of you know where the bug is plz tell me, thank you!!

---
+++ TestUniqueExistencePosition2.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class TestUniqueExistencePosition2 {
+  public static void main(String args[]) {
+    String word = "Hello";
+    int length=word.length();
+    String display="";
+    for(int i=0;i<length;i++){
+      display=display+"\uFE4D ";
+      System.out.print(display);
+    }
+}
+}
